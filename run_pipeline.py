655a48a9da630d02a36330fa64295d558a201196
Aman Kumar Rai <aman.kumar.rai1203@gmail.com>
2026-06-11 20:43:51 +0530

    Add files via upload

---
+++ run_pipeline.py
@@ -0,0 +1,7 @@
+print("Bluestock Mutual Fund Analytics Pipeline")
+
+print("ETL Completed")
+print("Dashboard Ready")
+print("Advanced Analytics Completed")
+
+print("Pipeline Executed Successfully")
